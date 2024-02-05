6d3f9e3e00dfbabb5be567cba9661be278ec0546
kSpets25 <142757766+kSpets25@users.noreply.github.com>
2024-02-05 16:41:48 -0500

    update js

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ var letters = [] //create alphabet array
   const alphabet = alpha.map((x) => String.fromCharCode(x));
   console.log(alphabet);
 
-document.onkeyup = function(e) {  //create key event
+/*document.onkeyup = function(e) {  //create key event
   console.log(e.key)
-}
+}*/
 
 var randomWord = words[Math.floor(Math.random() * words.length)]; //select random word from array/place in element
 console.log(randomWord)
@@ -40,38 +40,45 @@ console.log(chosenWord)
 var randomWord = document.querySelector('#word-to-guess');
 randomWord.innerHTML = chosenWord
 
+
 var remainingGuess; //when page opens - show remaining-quesses
  var guessCount = 0;
- var numberOfGuesses = 11;
+ var numberOfGuesses = 10;
  remainingAttemps = numberOfGuesses--;
- console.log(numberOfGuesses)
+ console.log(remainingAttemps)
 
 var remainingGuess = document.querySelector('#remaining-guesses'); //add value to document
-remainingGuess.innerHTML = numberOfGuesses
+remainingGuess.innerHTML = remainingAttemps
 
-document.onkeyup = function(e) {//create key event
-  console.log(e.key) }
+//document.onkeyup = function(e) {//create key event
+  //console.log(e.key) }
 
 //search and check letter when user presses a letter key - replace letter or do nothing.
 
-function updateletter() {
-  
-  //var chosenWord = chosenWord.split('_');//split the clue into an array
-  var chosenLet = document.onkkeyup(e.key)
-  for (var i = 0; i < chosenWord.length; i++){ //loop through the characters in thee word
-    if (chosenWord.charAt(i).toLowerCase() === chosenLet.toLowerCase()); { //if the letter matches the guess
-      chosenLet[i] = chosenWord.charAt(i).replace;  //replace the corresponding character in the word
-     }
-  
-    //chosenWord[i] = chosenWord.join('_');//convert the clue array back into a string
-    
-    
-    }
-    
+document.onkeyup = function(e) {  //create key event
+  console.log(e.key)
+//}
+console.log('beforeforloop')
+//function updateletter() {
+  var randomWord;
+  console.log(e.key)
+  var chosenLet = (e.key)
+  console.log('almostinforloop')
+  for (var i = 0; i < chosenWord.length; i++) {//loop through the characters in thee word
+    if (chosenWord.charAt(i).toLowerCase() === chosenLet.toLowerCase()){
+    console.log('infifloop', [i]);
+  }
+  console.log('inforloop')
 }
-console.log(chosenWord)
+console.log('outofforloop')
+    //if the letter matches the guess
+    chosenLet = chosenWord.charAt(i).replace('_');  //replace the corresponding character in the word
+  console.log('replaceLetter')
 
-
-
+      
+}    
+  
+  //chosenWord[i] = chosenWord.join('_');//convert the clue array back into a string
+  
+    
 //calculate and show score
-
